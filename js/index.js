a5fd8ec3cf4909840b71ec25ae6bc5de739ea15b
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-19 18:36:27 +0900

    refactor : 함수 명 변경

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const todosApp = () => {
   const $active = document.querySelector('.active');
   const $completed = document.querySelector('.completed');
 
-  const handleAddTodoList = (e) => {
+  const handleInputTodo = (e) => {
     if (e.key !== 'Enter') return;
     if (todoList.some((todo) => todo.name === e.target.value)) {
       alert('이미 존재하는 계획입니다.');
@@ -37,7 +37,7 @@ const todosApp = () => {
     $all.click();
   };
 
-  $inputTodo.addEventListener('keyup', handleAddTodoList);
+  $inputTodo.addEventListener('keyup', handleInputTodo);
 
   $todoList.addEventListener('click', (e) => {
     const $todoLi = e.target.parentNode.parentNode;
