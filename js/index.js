bdccc91541aba974ab8a8422253d30d6f503e0db
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-02-19 13:01:02 +0900

    feat : TEMPLATE을 사용해 HTML에 todo 리스트 추가

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
 const todosApp = () => {
   const todoList = [];
+  const TEMPLATE = (todo) =>
+    ` <li>
+        <div>
+          <input class="toggle" type="checkbox" />
+          <label class="label">${todo}</label>
+          <button class="destroy"></button>
+        </div>
+      </li>
+  `;
 
   const $inputTodo = document.querySelector('.new-todo');
 
   $inputTodo.addEventListener('keyup', (e) => {
     if (e.key !== 'Enter') return;
 
+    const $todoList = document.querySelector('#todo-list');
+
     todoList.push(e.target.value);
+    $todoList.insertAdjacentHTML('beforeend', TEMPLATE(e.target.value));
+    e.target.value = ''
   });
 };
 
